7ceab5668a5810812679bae8af0aed9e76745224
Velemyra <zhurbenkovasiliy@gmail.com>
2026-03-26 15:32:17 +0200

    Deploy ultra-minimal test version - 142KB bundle

---
+++ src/App-test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AppTest() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>🚀 Psychology of Power</h1>
+      <p>✅ App is working!</p>
+      <p>📱 PWA Ready</p>
+      <p>🎥 Camera Ready</p>
+    </div>
+  )
+}
+
+export default AppTest
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import AppTest from './App-test'
 import './index.css'
-
-// Import compatibility utilities
-import './utils/ios-compatibility.js'
-import './utils/windows-compatibility.js'
-
-// Service Worker disabled for local development
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js')
-//       .then((registration) => {
-//         console.log('SW registered: ', registration)
-//       })
-//       .catch((registrationError) => {
-//         console.log('SW registration failed: ', registrationError)
-//       })
-//   })
-// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppMinimal />
-    </BrowserRouter>
-  </React.StrictMode>
+    <AppTest />
+  </React.StrictMode>,
 )
